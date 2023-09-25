ab1f6159b498a8aeeb83d1a776c7350cee8ba4b3
Ogelnac <jbhughes1999@gmail.com>
2023-09-26 00:04:32 +0100

    Updated colour for dialogue boxes

--- src/cutscene.rs
+++ src/cutscene.rs
@@ -100,7 +100,7 @@ fn spawn_cutscene(
 		.spawn((SpriteBundle {
 			transform: Transform::from_xyz(150.0, -250.0, 600.0),
 			sprite: Sprite {
-				color: Color::hex("DA9F56").unwrap(),
+				color: Color::hex("EDD6AD").unwrap(),
 				custom_size: Some(Vec2::new(TEXT_BOX_WIDTH, TEXT_BOX_HEIGHT)),
 				..Default::default()
 			},
